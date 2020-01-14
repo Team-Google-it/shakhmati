6cf0077b9952e9742d6f552d4a59eb139036cdb6
Michael Dunphy <michaeldunphy52@gmail.com>
2020-01-14 22:24:19 +0000

    bootstrap adjustments

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,10 @@
 //
 //= require rails-ujs
 //= require activestorage
+//= require jquery
+//= require jquery_ujs
 //= require turbolinks
-//= require_tree .
-//= require jquery3
 //= require popper
+//= require tether
 //= require bootstrap-sprockets
+//= require_tree .
