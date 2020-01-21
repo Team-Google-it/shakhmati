fa915e968395d84a7a09360e8462146eea934bdc
PickleBanquet <54336908+PickleBanquet@users.noreply.github.com>
2020-01-21 08:59:42 -0600

    Update game_spec.rb

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,16 +1,5 @@
-#require 'rails_helper'
-#
-#RSpec.describe Game, type: :model do
-#
-#	describe "#is_obstructed?" do
-#		it "should return false if piece is not obstructed diagonally" do
-#			g = Game.create!()
-#			Bishop.create!(game_id: g.id, x_position: 0, y_position: 5)
-#			Pawn.create!(game_id: g.id, x_position: 2, y_position: 3)
-#
-#			result = g.is_obstructed?(0,5,2,3)
-#			expect(result).to be false
-#		end
-#	end
-#
-#end
+require 'rails_helper'
+
+RSpec.describe Game, type: :model do
+
+end
