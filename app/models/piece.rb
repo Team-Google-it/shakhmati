c1e1f464d9d6d9effaf717b16942861a51c6da94
Michael Dunphy <michaeldunphy52@gmail.com>
2020-01-22 23:43:52 +0000

    horizontal and diagonal

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -5,8 +5,12 @@ class Piece < ApplicationRecord
 		case 
 			when vertical_move?(x_target, y_target)
 				vertical_obstruction?(y_target)
+			when horizontal_move?(x_target, y_target)
+				horizontal_obstruction?(x_target)
+			when diagonal_move?(x_target, y_target)
+				diagonal_obstruction?(x_target, y_target)
 			else
-				false
+				raise 'Invalid Input'
 		end
 	end
 
@@ -14,10 +18,36 @@ class Piece < ApplicationRecord
 		x_position == x_target && y_position != y_target
 	end
 
+	def horizontal_move?(x_target, y_target)
+		x_position != x_target && y_position == y_target
+	end
+
+	def diagonal_move?(x_target, y_target)
+		(x_target - x_position).abs == (y_target - y_position).abs
+	end
+
 	def vertical_obstruction?(y_target)
 		direction = y_target > y_position ? 1 : -1
 		(y_position + direction).step(y_target - direction, direction) do |y_current|
 			return true if occupied?(x_position, y_current)
+		end
+		false
+	end
+
+	def horizontal_obstruction?(x_target)
+		direction = x_target > x_position ? 1 : -1
+		(x_position + direction).step(x_target - direction, direction) do |x_current|
+			return true if occupied?(x_current, y_position)
+		end
+		false
+	end
+
+	def diagonal_obstruction?(x_target, y_target)
+		x_direction = x_target > x_position ? 1 : -1
+		y_direction = y_target > y_target ? 1 : -1
+		(x_position + x_direction).step(x_target - x_direction, x_direction) do |x_current|
+			y_current = y_position + ((x_current - x_position).abs * y_direction)
+			return true if occupied?(x_current, y_current)
 		end
 		false
 	end
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -19,6 +19,47 @@ RSpec.describe Piece, type: :model do
 			result = p.is_obstructed?(0,3)
 			expect(result).to eq(false)
 		end
-	end
 
+		it "should return true if piece path is obstructed horizontally" do
+			g = Game.create!()
+			p = Rook.create!(game_id: g.id, x_position: 0, y_position: 0)
+			Knight.create!(game_id: g.id, x_position: 1, y_position: 0)
+
+			result = p.is_obstructed?(3,0)
+			expect(result).to eq(true)
+		end
+
+		it "should return false if piece path is not obstructed horizontally" do
+			g = Game.create!()
+			p = Rook.create!(game_id: g.id, x_position: 0, y_position: 0)
+
+			result = p.is_obstructed?(3,0)
+			expect(result).to eq(false)
+		end
+
+		it "should return true if piece path is obstructed diagonally" do
+			g = Game.create!()
+			p = Bishop.create!(game_id: g.id, x_position: 0, y_position: 5)
+			Pawn.create!(game_id: g.id, x_position: 1, y_position: 4)
+
+			result = p.is_obstructed?(3,2)
+			expect(result).to eq(true)
+		end
+
+		it "should return false if piece path is not obstructed diagonally" do
+			g = Game.create!()
+			p = Bishop.create!(game_id: g.id, x_position: 0, y_position: 5)
+
+			result = p.is_obstructed?(3,2)
+			expect(result).to eq(false)
+		end
+
+		it "should raise an error message if piece makes an invalid move" do
+			g = Game.create!()
+			p = Knight.create!(game_id: g.id, x_position: 3, y_position: 3)
+
+			result = p.is_obstructed?(1,4)
+			expect{ result }.to raise_error
+		end
+	end
 end
