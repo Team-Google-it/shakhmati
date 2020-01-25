04a9f8fb5caf4af393a86e871a83e5af3fbfc292
Michael Dunphy <michaeldunphy52@gmail.com>
2020-01-25 02:11:36 +0000

    king valid move

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 2020_01_25_015626) do
     t.integer "y_position"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "color"
   end
 
   create_table "users", force: :cascade do |t|
