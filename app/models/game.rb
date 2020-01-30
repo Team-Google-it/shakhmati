0b44f297c3fa7f6ffeced7c54f501704eefd0463
Michael Dunphy <michaeldunphy52@gmail.com>
2020-01-30 01:54:40 +0000

    bug fix

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,8 +3,6 @@ class Game < ApplicationRecord
 
 	has_many :pieces
 
-	belongs_to :user
-
 	scope :available, -> { where("white_player_id IS NULL or black_player_id IS NULL")}
 
 	def populate_white_pieces
