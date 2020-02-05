e6123afdb6e66a23268ca44b1fde9425ee008ae0
derfman9303 <frederichodges@gmail.com>
2020-02-05 23:50:54 +0000

    added valid move logic to be used in piece controller

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,6 +1,6 @@
 class PiecesController < ApplicationController
   skip_before_action :verify_authenticity_token
-  
+
   def index
 
   end
@@ -10,13 +10,16 @@ class PiecesController < ApplicationController
   end
 
   def update
-
     @piece = Piece.find_by(id: params[:id])
     new_x = params[:x_position].to_i
     new_y = params[:y_position].to_i
-
-    @piece.update_position(new_x, new_y)
-
+    if @piece.valid_move? == false
+      flash[:alert] = 'This move is invalid. Try again.'
+    elsif @piece.is_obstructed?(new_x, new_y) == true
+      flash[:alert] = 'This move is blocked. Try again.'
+    else
+      @piece.update_position(new_x, new_y)
+    end
   end
 
   private
