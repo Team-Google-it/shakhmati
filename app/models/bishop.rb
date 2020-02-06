08ac3d891d97645dafe212441992616adb2b0fd4
Michael Dunphy <michaeldunphy52@gmail.com>
2020-02-06 00:28:38 +0000

    tests

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,5 +1,5 @@
 class Bishop < Piece
-	def move_valid?(x_target, y_target)
+	def valid_move?(x_target, y_target)
       return false if same_position?(x_target, y_target)
       return false if !on_board?(x_target, y_target)
       return false if is_obstructed?(x_target, y_target)
