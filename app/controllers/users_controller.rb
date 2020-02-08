f3dcaeda598a5f7aead9870e7e66e71302b78d7d
derfman9303 <frederichodges@gmail.com>
2020-02-08 05:37:09 +0000

    fixed a typo in the controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user_games = Game.where(:white_player_id == current_user.id || :black_playeer_id == current_user.id).all
+    @user_games = Game.where(:white_player_id == current_user.id || :black_player_id == current_user.id).all
   end
   def update
 
