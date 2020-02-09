512361d64a9ae49a0e111c1393de0b43a26f4b9e
derfman9303 <frederichodges@gmail.com>
2020-02-09 00:35:54 +0000

    added functional forfeit button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user_games = Game.where(:white_player_id == current_user.id).or(Game.where(:black_player_id == current_user.id))
+    @user_games = Game.where(:white_player_id => current_user.id).or(Game.where(:black_player_id => current_user.id))
   end
   def update
 
