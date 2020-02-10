927ac364f671836f5b6ab4df572b61b88f53792f
Michael Dunphy <michaeldunphy52@gmail.com>
2020-02-10 22:08:55 +0000

    checking

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,6 +5,14 @@ class Game < ApplicationRecord
 
 	scope :available, -> { where("white_player_id IS NULL or black_player_id IS NULL")}
 
+	def in_check?(color)
+		king = get_piece('King', color)
+		opponent_pieces(color).each do |opponent|
+			return true if opponent.valid_move?(king.x_position, king.y_position)
+		end
+		false
+	end
+
 	def populate_white_pieces
 		(0..7).each do |num|
 			Pawn.create(game_id: self.id, x_position: num, y_position: 1, player_id: self.white_player_id, color: "white")
@@ -37,6 +45,18 @@ class Game < ApplicationRecord
 
 		Queen.create(game_id: self.id, x_position: 3, y_position: 7, player_id: self.black_player_id, color: "black")
 		King.create(game_id: self.id, x_position: 4, y_position: 7, player_id: self.black_player_id, color: "black")
+	end
+
+	def get_piece(type, color)
+		pieces.where(type: type, color: color).first
+	end
+
+	def opponent_color(color)
+		color == "white" ? "black" : "white"
+	end
+
+	def opponent_pieces(color)
+		pieces_where(color: opponent_color(color))
 	end
 
 end
