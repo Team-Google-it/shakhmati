6db62672ef857761c5d3b2364c7890a5a608511f
derfman9303 <frederichodges@gmail.com>
2020-02-23 17:35:27 +0000

    fixed can_be_captured not using would_be_in_check

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -92,7 +92,7 @@ class Piece < ApplicationRecord
 				game.swap_turn
 	  		pieces.each do |opponent|
 					puts "can it be?"
-	  			if opponent.valid_move?(x_current, y_current)
+	  			if opponent.valid_move?(x_current, y_current) && !opponent.would_be_in_check?(x_current, y_current)
 						return true
 					end
 	  		end
@@ -138,15 +138,19 @@ class Piece < ApplicationRecord
   	def checkmate?
   		checked_king = game.pieces.where(type: 'King', color: opponent_color).first
   		unless checking?(color)
+				puts "checking"
 				return false
 			end
   		if can_be_captured?(x_position, y_position)
+				puts "can be captured"
 				return false
 			end
   		if checked_king.can_move_out_of_check?(checked_king.x_position, checked_king.y_position)
+				puts "can move out of check"
 				return false
 			end
   		if can_be_blocked?(checked_king.x_position, checked_king.y_position)
+				puts "can be blocked"
 				return false
 			end
   		true
