02c20b9a5dcb2633efdbd694dcbc9ca39f413b8a
PickleBanquet <54336908+PickleBanquet@users.noreply.github.com>
2020-03-03 15:50:21 -0600

    Fix errors

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -26,25 +26,22 @@ class PiecesController < GamesController
     new_y = params[:y_position].to_i
 
 
-    if (current_user.id == @game.white_player_id && @game.turn == "white") || (current_user.id == @game.black_player_id && @game.turn == "black")    
-      if current_user.id == @piece.player_id
-        flash.now.alert = 'This move is invalid. Try again.'
-      else
-        flash.now.alert = 'It is not your turn!'
+    if (current_user.id == @game.white_player_id && @game.turn == "white") || (current_user.id == @game.black_player_id && @game.turn == "black")
+    # if (current_user.id == @game.white_player_id && @game.turn == "white") || (current_user.id == @game.black_player_id && @game.turn == "black")
+      if @piece.move_to(new_x, new_y) == false
+        if current_user.id == @piece.player_id
+          flash.now.alert = 'This move is invalid. Try again.'
+          render partial: 'games/modal'
+        end
       end
     else
       flash.now.alert = 'It is not your turn!'
       render partial: 'games/update'
     end
-    else
-      @game.swap_turn
-      render partial: 'games/modal'
-      # action cable broadcast to channel
-      if @piece.save
-        ActionCable.server.broadcast 'game_channel',
-        reload: true
-      end
-    end
+    #else
+    #  flash.now.alert = 'It is not your turn!'
+    #  render partial: 'games/update'
+    #end
   end
 
   private
